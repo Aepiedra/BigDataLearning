f5bb962b6ebc56afd13da6148c201182d74b865d
Aepiedra <angelpiedra98@gmail.com>
2022-06-16 22:19:55 -0500

    Iniciando

---
+++ main.py
@@ -0,0 +1,14 @@
+from unittest import result
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("data/Dataset100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+results = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(results.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
